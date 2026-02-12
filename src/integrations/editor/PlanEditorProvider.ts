d946a7a79e0d65800a75b718e2228edbf00f7291
code-crusher <vatsal.bajpai05@gmail.com>
2026-02-12 12:26:33 +0530

    fix: address PR review comments
    - Remove commented-out code in Task.ts
    - Replace Math.random() with crypto.randomBytes() for CSP nonce

--- src/integrations/editor/PlanEditorProvider.ts
+++ src/integrations/editor/PlanEditorProvider.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode"
 import * as path from "path"
 import * as fs from "fs/promises"
+import * as crypto from "crypto"
 
 import { getPlanMemoryDirectoryPath } from "../../utils/storage"
 
@@ -517,12 +518,7 @@ export class PlanEditorProvider implements vscode.CustomTextEditorProvider {
 	}
 
 	private getNonce(): string {
-		let text = ""
-		const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-		for (let i = 0; i < 32; i++) {
-			text += possible.charAt(Math.floor(Math.random() * possible.length))
-		}
-		return text
+		return crypto.randomBytes(16).toString("hex")
 	}
 }
 
